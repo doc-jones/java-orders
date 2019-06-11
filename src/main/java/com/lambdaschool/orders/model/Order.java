41691e2d9b5f16f53eeac4270465a472b1685dea
doc-jones <37349558+doc-jones@users.noreply.github.com>
2019-06-11 00:32:47 -0400

    Add model/Order

--- src/main/java/com/lambdaschool/orders/model/Order.java
+++ src/main/java/com/lambdaschool/orders/model/Order.java
@@ -1,4 +1,12 @@
 package com.lambdaschool.orders.model;
 
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+
+@Table(name = "order")
 public class Order {
+    private long orderid;
 }
